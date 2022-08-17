11137a2694c8e32a0c024e95035b9c426423d1b9
Paridokht Alavinia <paridokht.alavinia@gmail.com>
2022-08-17 22:19:39 +0200

    adding test and optimization to components

--- packages/common-components/rollup.config.js
+++ packages/common-components/rollup.config.js
@@ -3,6 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 const packageJson = require("./package.json");
 
 export default [
@@ -21,9 +25,14 @@ export default [
       },
     ],
     plugins: [
+      
+      peerDepsExternal(),
+
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      
+      terser(),
     ],
   },
   {
